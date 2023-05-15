276647aa3bdea5a208b25ee02848c3573e26acd0
DarianGrabino <dariangra@gmail.com>
2023-05-15 07:09:44 -0700

    fix betty

--- interprete.c
+++ interprete.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * _isdigit - checks if a string is a number
+ * @c: character to check
+ * Return: 1 if its a number, 0 otherwise
+*/
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
@@ -11,7 +16,12 @@ int _isdigit(int c)
 		return (0);
 	}
 }
-
+/**
+ * push - function to add a new element to the top of the stack
+ * @stack: pointer to pointer to the first element on the stack
+ * @line_number: number of the current line
+ * Return: 0 if the element is successfully added to the stack, or an error messange
+*/
 int push(stack_t **stack, unsigned int line_number)
 {
     char *numchar = NULL;
@@ -53,7 +63,11 @@ int push(stack_t **stack, unsigned int line_number)
     *stack = new_node;
     return(0);
 }
-
+/**
+ * pall - print all elements in the stack
+ * @stack: pointer to pointer to the first element on the stack
+ * Return: 0 if the stack prints successfully, or if the stack is empty
+*/
 int pall(stack_t **stack)
 {
     stack_t *current = *stack;
@@ -73,7 +87,12 @@ int pall(stack_t **stack)
 	}
     return(0);
 }
-
+/**
+ * main - program input
+ * @argc: number of arguments passed on the command line
+ * @argc: index of strings containing the arguments passed on the command line
+ * Return: 0 if the program runs correctly
+*/
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -81,48 +100,45 @@ int main(int argc, char **argv)
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
-
-stack_t *stack = NULL;
-FILE *file = fopen(argv[1], "r");
-if (file == NULL)
-{
-    fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-    exit(EXIT_FAILURE);
-}
-
-char *buffer = NULL, *code = NULL;
-size_t buffsize = 0;
-unsigned int line_number = 0;
-
-while (getline(&buffer, &buffsize, file) != -1)
-{
-    line_number++;
-    code = strtok(buffer, " \t\n");
-    if (code == NULL)
+    stack_t *stack = NULL;
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
     {
-        continue;
-    }
-    if (strcmp(code, "push") == 0)
-    {
-        push(&stack, line_number);
-    }
-    else if (strcmp(code, "pall") == 0)
-    {
-        pall(&stack);
-    }
-    else
-    {
-        fprintf(stderr, "L%u: unknown instruction %s\n", line_number, code);
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-}
-fclose(file);
-while (stack != NULL)
-{
-    stack_t *next_node = stack -> next;
-    free(stack);
-    stack = next_node;
-}
-free(buffer);
-return (0);
+    char *buffer = NULL, *code = NULL;
+    size_t buffsize = 0;
+    unsigned int line_number = 0;
+    while (getline(&buffer, &buffsize, file) != -1)
+    {
+        line_number++;
+        code = strtok(buffer, " \t\n");
+        if (code == NULL)
+        {
+            continue;
+        }
+        if (strcmp(code, "push") == 0)
+        {
+            push(&stack, line_number);
+        }
+        else if (strcmp(code, "pall") == 0)
+        {
+            pall(&stack);
+        }
+        else
+        {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, code);
+            exit(EXIT_FAILURE);
+        }
+    }
+    fclose(file);
+    while (stack != NULL)
+    {
+        stack_t *next_node = stack -> next;
+        free(stack);
+        stack = next_node;
+    }
+    free(buffer);
+    return (0);
 }
